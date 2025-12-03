06376dc961d3d8684273c8bbefcee7baecdc3244
Tiago Soares de Carvalho <devtiagosoaresc@gmail.com>
2025-12-02 23:45:56 -0300

    codigo inical

---
+++ script.js
@@ -0,0 +1,12 @@
+// Elementos DOM
+const inputSearch = document.getElementById("input-search") 
+const btnSearch = document.getElementById("btn-search")
+const profileResults = document.querySelector(".profile-results")
+
+// Evento listener no botão
+
+
+
+// Pegando o valor do input no qual o usuário foi digitado
+const userSearched = inputSearch.value 
+console.log(inputSearch)
